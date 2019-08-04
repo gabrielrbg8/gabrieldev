b288a399f64c36b00a06f4f997bd4d68e190e9e0
gabrielrbg8 <gabrielrbg8@outlook.com>
2019-08-04 20:21:39 -0300

    fc

---
+++ src/js/js.js
@@ -0,0 +1,15 @@
+$(document).scroll(function() {
+    var y = $(this).scrollTop();
+    if (y > 700) {
+      $('.navbar')
+        .css({
+          'display': 'flex',
+          'background' : 'white'
+        });
+    } else {
+      $('.navbar')
+        .css({
+          'display': 'none',
+        });
+    }
+  });
